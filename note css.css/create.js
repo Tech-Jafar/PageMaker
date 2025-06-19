25fcd76e362092c1ce81c50fb563bb3e1740eee2
Jafar Iqbal <jafar.iqbal.jobs@gmail.com>
2025-06-19 14:30:47 +0530

    Add files via upload

---
+++ note css.css/create.js
@@ -0,0 +1,78 @@
+
+//head
+function fontpx(pxsize) {
+    document.getElementById('head-section').style.fontSize = pxsize;
+}
+
+function fontem(size) {
+    document.getElementById('head-section').style.fontStyle = size;
+}
+
+function back(backcolor) {
+    document.getElementById('head-section').style.backgroundColor = backcolor;
+}
+
+function weight(wet) {
+    document.getElementById('head-section').style.fontWeight = wet;
+}
+
+
+// data
+function fontemm(remsize) {
+    document.getElementById('data-secation').style.fontSize = remsize;
+}
+
+function copy(sizze) {
+    document.getElementById('data-secation').style.fontStyle = sizze;
+}
+
+function backk(backkcolor) {
+    document.getElementById('data-secation').style.backgroundColor = backkcolor;
+}
+
+function weightt(wett) {
+    document.getElementById('data-secation').style.fontWeight = wett;
+}
+
+//image
+
+function proimage(proage) {
+    document.getElementById('horse').src = proage;
+}
+
+//containt
+
+function fontemmm(remmsize) {
+    document.getElementById('containt-hold').style.fontSize = remmsize;
+}
+
+function copyy(sizzee) {
+    document.getElementById('containt-hold').style.fontStyle = sizzee;
+}
+
+function bacckkk(backkcoloor) {
+    document.getElementById('containt-hold').style.backgroundColor = backkcoloor;
+}
+
+function wweightt(wwett) {
+    document.getElementById('containt-hold').style.fontWeight = wwett;
+}
+
+//footer    
+
+function footerbye(coco) {
+    document.getElementById('footer-deco').style.fontSize = coco;
+}
+
+function fotcopy(fotcopyes) {
+    document.getElementById('footer-deco').style.fontStyle = fotcopyes;
+}
+
+function fotdeco(deco) {
+    document.getElementById('footer-deco').style.backgroundColor = deco;
+}
+
+
+function weeightt(weett) {
+    document.getElementById('footer-deco').style.fontWeight = weett;
+}
